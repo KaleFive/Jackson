644fc3ee716367d3ae776eb0b288dcf24c673636
Kim Leung <kleung@contently.com>
2018-03-27 19:49:44 -0400

    add octokit and trying to get json payload

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+const Octokit = require('octokit');
+const secrets = require("./secrets");
+const gh = Octokit.new({
+    token: secrets.octokitToken
+});
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(bodyParser.json());
 
-app.post('/', (req, res) => res.send('Posting World!'))
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/', (req, res) => {
+  let repo = gh.getRepo("kalefive", "testNode")
+  debugger
+  console.log(req.body)
+  res.send('Posting World!')
+})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
