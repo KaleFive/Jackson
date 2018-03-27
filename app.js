c141467a2517b6f58a3e96a11a9e60828727e082
Kim Leung <kleung@contently.com>
2018-03-27 18:46:47 -0400

    add post method endpoint

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-console("testing")
+app.post('/', (req, res) => res.send('Posting World!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
