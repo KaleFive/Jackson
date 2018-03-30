c820b76c1da83b852f7278be9f09353e7bdfeef7
Lo Leung <kalefive.me@gmail.com>
2018-03-30 09:45:33 -0400

    run blink diff tool:

--- imageMagick.js
+++ imageMagick.js
@@ -1,6 +1,7 @@
 const AWS = require("aws-sdk")
 const s3 = new AWS.S3()
 const fs = require("fs")
+const blinkDiff = require("blink-diff")
 
 let bucket = "kalefive.unique.bucket.name"
 let key = "cnnImage.png"
@@ -10,4 +11,28 @@ function pullMasterS3Image() {
   let image = fs.createWriteStream("./writtenFile.png")
   s3.getObject(params).createReadStream().pipe(image)
 }
-pullMasterS3Image()
+// pullMasterS3Image()
+
+function runBlinkDiff() {
+  let diff = new blinkDiff({
+    // imageAPath: "./Screenshots/master/" + page + ".png",
+    // imageBPath: "./Screenshots/new/" + page + ".png",
+    imageAPath: "./master.png",
+    imageBPath: "./qa.png",
+
+    // thresholdType: blinkDiff.THRESHOLD_PERCENT,
+    // threshold: 0.01, // 1% threshold
+
+    imageOutputPath: "./diffResult.png"
+  })
+
+  diff.run(function (error, result) {
+    if (error) {
+      throw error
+    } else {
+      console.log(diff.hasPassed(result.code) ? "Passed" : "Failed")
+      console.log("Found " + result.differences + " differences.")
+    }
+  })
+};
+runBlinkDiff()
